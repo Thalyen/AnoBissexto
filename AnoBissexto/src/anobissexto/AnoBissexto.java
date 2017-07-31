87b72b212bfe5da741df0b2525b85ab32615844d
Tatiana Dantas <tatiana_dantas@ymail.com>
2017-07-31 20:12:01 -0300

    add implementação para testeClasseValida 1 entrada 4

---
+++ AnoBissexto/src/anobissexto/AnoBissexto.java
@@ -0,0 +1,16 @@
+package anobissexto;
+
+/**
+ *
+ * @author Tat
+ */
+public class AnoBissexto {
+
+    public boolean AnoBissexto(int ano) {
+        if(ano%4 ==0)
+            return true;
+                    else
+            return false;
+    }
+
+}
